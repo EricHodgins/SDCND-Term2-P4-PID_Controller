651965248d68be863b687b1a6b88e64e6a98f423
EricHodgins <hodgins.e@gmail.com>
2018-08-26 23:57:09 -0400

    fixed formatting.

--- src/main.cpp
+++ src/main.cpp
@@ -38,7 +38,6 @@ int main()
   pid.Init(0.125, 0.000156, 3.09);
 
   pid.is_twiddle_on = twiddle;
-  //pid.Init(0.134611, 0.000270736, 3.05349);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
